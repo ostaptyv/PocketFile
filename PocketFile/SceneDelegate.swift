2c3fb0f33918b996c47d088b0652066e1f01b832
Ostap Tyvonovych <ostaptyv@gmail.com>
2022-06-07 17:21:44 +0300

    Initial commit

---
+++ PocketFile/SceneDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SceneDelegate.swift
+//  PocketFile
+//
+//  Created by Ostap Tyvonovych on 07.06.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+}
+
